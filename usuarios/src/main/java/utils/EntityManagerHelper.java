be7509c66150ca48ce69ab852f2d68b4bc412035
SwagDogeMan <juan.aledos@um.es>
2026-04-29 14:46:42 +0200

    Usuario - persistence.xml arreglo

--- usuarios/src/main/java/utils/EntityManagerHelper.java
+++ usuarios/src/main/java/utils/EntityManagerHelper.java
@@ -3,13 +3,32 @@ package utils;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntityManagerHelper {
     private static EntityManagerFactory entityManagerFactory;
     private static final ThreadLocal<EntityManager> entityManagerHolder;
 
     static {
-        entityManagerFactory = Persistence.createEntityManagerFactory("usuarios");
+        Map<String, Object> overrides = new HashMap<>();
+
+        String[] props = {
+            "javax.persistence.jdbc.url",
+            "javax.persistence.jdbc.user",
+            "javax.persistence.jdbc.password",
+            "javax.persistence.jdbc.driver"
+        };
+
+        for (String prop : props) {
+            String envKey = prop.toUpperCase().replace('.', '_');
+            String value = System.getenv(envKey);
+            if (value != null && !value.isBlank()) {
+                overrides.put(prop, value);
+            }
+        }
+
+        entityManagerFactory = Persistence.createEntityManagerFactory("usuarios", overrides);
         entityManagerHolder = new ThreadLocal<>();
     }
 
