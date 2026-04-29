f3750a31575831ca873913de4b16a13f847b08d9
SwagDogeMan <juan.aledos@um.es>
2026-04-29 17:05:17 +0200

    Compraventa - OpenAPI ya entrega

---
+++ compraventas-spring/src/main/java/compraventas/infraestructura/rest/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package compraventas.infraestructura.rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Servicio de Compraventas")
+                .description("Gestión de compraventas de productos de segunda mano")
+                .version("1.0.0"));
+    }
+}
