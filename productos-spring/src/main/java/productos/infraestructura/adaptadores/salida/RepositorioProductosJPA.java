6c5363baa1350f25f81629ced47760db5ae437d7
SwagDogeMan <juan.aledos@um.es>
2026-04-30 17:42:33 +0200

    Arreglos menores Productos

--- productos-spring/src/main/java/productos/infraestructura/adaptadores/salida/RepositorioProductosJPA.java
+++ productos-spring/src/main/java/productos/infraestructura/adaptadores/salida/RepositorioProductosJPA.java
@@ -1,8 +1,11 @@
 package productos.infraestructura.adaptadores.salida;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import productos.aplicacion.puertos.salida.IRepositorioProductos;
+import productos.dominio.modelo.Producto;
 
 @Repository
-public interface RepositorioProductosJPA extends IRepositorioProductos {
+public interface RepositorioProductosJPA
+        extends IRepositorioProductos, JpaRepository<Producto, String> {
 }
